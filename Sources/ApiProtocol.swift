df63b02fb7eb4d8ac36a564176a58ca7d00adf89
Kirill Averyanov <Kirillzzy@gmail.com>
2017-02-10 23:27:07 +0300

    api temporary

---
+++ Sources/ApiProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  ApiProtocol.swift
+//  SocialMilkServer
+//
+//  Created by Kirill Averyanov on 10/02/2017.
+//
+//
+
+import Foundation
+
+protocol ApiProtocol{
+    static var token: String {get set}
+    
+    
+}
---
+++ Sources/ApiVkManager.swift
@@ -0,0 +1,14 @@
+//
+//  ApiVkManager.swift
+//  SocialMilkServer
+//
+//  Created by Kirill Averyanov on 10/02/2017.
+//
+//
+
+import Foundation
+
+
+final class ApiVkManager: ApiProtocol{
+    static var token: String = ""
+}
